ccc6b4ead97920b387f38541baa708b89d4cad70
BTCnake <btcnake@gmail.com>
2026-09-22 05:41:27 +0200

    Deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [svelte()],
+    base: "/bitcoinpayserver.github.io/"
 })
