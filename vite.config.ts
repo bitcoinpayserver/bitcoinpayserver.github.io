e49509c9bd834f814a2ede02afc2514731bbf12a
BTCnake <btcnake@gmail.com>
2026-09-22 14:55:53 +0200

    Base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [svelte()],
-    base: "/bitcoinpayserver.github.io/"
+    base: 'https://bitcoinpayserver.github.io'
 })
